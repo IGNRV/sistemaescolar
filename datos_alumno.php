c68f1a848822ce74f508753e4d8323e8f7069797
Ubuntu <ubuntu@vps-b2b2bbac.vps.ovh.ca>
2023-11-26 15:50:55 +0000

    Avance

--- datos_alumno.php
+++ datos_alumno.php
@@ -1,3 +1,58 @@
+<?php
+// Incluye la conexión a la base de datos
+require_once 'db.php';
+
+// Inicia sesión
+session_start();
+
+// Verifica si el usuario está logueado y obtiene su id
+if (!isset($_SESSION['correo_electronico'])) {
+    header('Location: login.php');
+    exit;
+} else {
+    $correo_electronico = $_SESSION['correo_electronico'];
+    // Busca el id del usuario en la tabla 'usuarios'
+    $queryUsuario = "SELECT id FROM usuarios WHERE correo_electronico = '$correo_electronico'";
+    $resultadoUsuario = $conn->query($queryUsuario);
+    if ($resultadoUsuario->num_rows > 0) {
+        $usuario = $resultadoUsuario->fetch_assoc();
+        $id_usuario = $usuario['id'];
+    } else {
+        // Manejar el error si el usuario no se encuentra
+        echo "Usuario no encontrado.";
+        exit;
+    }
+}
+
+// Verifica si se ha enviado el formulario
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['actualizar'])) {
+    // Recoge los datos del formulario
+    $nombre = $conn->real_escape_string($_POST['nombre']);
+    $telefono = $conn->real_escape_string($_POST['telefono']);
+
+    // Actualiza los datos en la base de datos
+    $updateQuery = "UPDATE alumno SET nombre = '$nombre', telefono = '$telefono' WHERE id_usuario = $id_usuario";
+    if ($conn->query($updateQuery) === TRUE) {
+        echo "Datos actualizados correctamente.";
+    } else {
+        echo "Error al actualizar los datos: " . $conn->error;
+    }
+}
+
+// Busca los datos del alumno que coincidan con el id_usuario
+$queryAlumno = "SELECT * FROM alumno WHERE id_usuario = $id_usuario";
+$resultadoAlumno = $conn->query($queryAlumno);
+
+if ($resultadoAlumno->num_rows > 0) {
+    $alumno = $resultadoAlumno->fetch_assoc();
+} else {
+    echo "Datos del alumno no encontrados.";
+    exit;
+}
+
+// A continuación va el código HTML...
+?>
+
 <h1 class="text-center">Datos del alumno</h1>
             <!-- Formulario de datos del alumno -->
             <form action="" method="post">
@@ -8,55 +63,51 @@
                 </div>
                 <div class="form-group">
                     <label>Fecha de Nacimiento:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['fecha_de_nacimiento']; ?>" disabled>
+                    <input type="text" class="form-control" name="fecha_de_nacimiento" value="<?php echo $alumno['fecha_de_nacimiento']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>RDA:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['rda']; ?>" disabled>
+                    <input type="text" class="form-control" name="rda" value="<?php echo $alumno['rda']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Apellido Paterno:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['appaterno']; ?>" disabled>
+                    <input type="text" class="form-control" name="apellido_paterno" value="<?php echo $alumno['apellido_paterno']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Apellido Materno:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['apmaterno']; ?>" disabled>
-                </div>
-                <div class="form-group">
-                    <label>Nombres:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['nombres']; ?>" disabled>
+                    <input type="text" class="form-control" name="apellido_materno" value="<?php echo $alumno['apellido_materno']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Calle:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['calle']; ?>" disabled>
+                    <input type="text" class="form-control" name="calle" value="<?php echo $alumno['calle']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Número:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['numero']; ?>" disabled>
+                    <input type="text" class="form-control" name="n_calle" value="<?php echo $alumno['n_calle']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Resto Dirección:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['resto_direccion']; ?>" disabled>
+                    <input type="text" class="form-control" name="resto_direccion" value="<?php echo $alumno['resto_direccion']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Villa/Población:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['villa_poblacion']; ?>" disabled>
+                    <input type="text" class="form-control" name="villa_poblacion" value="<?php echo $alumno['villa_poblacion']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Comuna:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['comuna']; ?>" disabled>
+                    <input type="text" class="form-control" name="comuna" value="<?php echo $alumno['comuna']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Ciudad:</label>
-                    <input type="text" class="form-control" value="<?php echo $alumno['ciudad']; ?>" disabled>
+                    <input type="text" class="form-control" name="ciudad" value="<?php echo $alumno['ciudad']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Email:</label>
-                    <input type="email" class="form-control" value="<?php echo $alumno['email']; ?>" disabled>
+                    <input type="email" class="form-control" name="correo_electronico" value="<?php echo $alumno['correo_electronico']; ?>" disabled>
                 </div>
                 <div class="form-group">
                     <label>Número de teléfono:</label>
-                    <input type="text" class="form-control" name="numero_de_telefono" value="<?php echo $alumno['numero_de_telefono']; ?>">
+                    <input type="text" class="form-control" name="telefono" value="<?php echo $alumno['telefono']; ?>">
                 </div>
                 <!-- Botón de actualizar con clase Bootstrap y personalizada -->
                 <button type="submit" class="btn btn-primary btn-block custom-button" name="actualizar">Actualizar</button>
--- padres_apoderados.php
+++ padres_apoderados.php
@@ -1,3 +1,39 @@
+<?php
+if(isset($_POST['actualizar_datos'])){
+    // Conectar a la base de datos
+    require_once 'db.php';
+
+    // Recuperar los valores del formulario
+    $rut = $conn->real_escape_string($_POST['rut']);
+    $parentesco = $conn->real_escape_string($_POST['parentesco']);
+    $nombre = $conn->real_escape_string($_POST['nombre']);
+    $apellidoPaterno = $conn->real_escape_string($_POST['apellidoPaterno']);
+    $apellidoMaterno = $conn->real_escape_string($_POST['apellidoMaterno']);
+    $telefonoParticular = $conn->real_escape_string($_POST['telefonoParticular']);
+    $telefonoTrabajo = $conn->real_escape_string($_POST['telefonoTrabajo']);
+    $correoElectronicoPersonal = $conn->real_escape_string($_POST['correoElectronicoPersonal']);
+    $correoElectronicoTrabajo = $conn->real_escape_string($_POST['correoElectronicoTrabajo']);
+    $calle = $conn->real_escape_string($_POST['calle']);
+    $n_calle = $conn->real_escape_string($_POST['n_calle']);
+    $restoDireccion = $conn->real_escape_string($_POST['restoDireccion']);
+    $villaPoblacion = $conn->real_escape_string($_POST['villaPoblacion']);
+    $comuna = $conn->real_escape_string($_POST['comuna']);
+    $ciudad = $conn->real_escape_string($_POST['ciudad']);
+    $tutorAcademico = isset($_POST['tutorAcademico']) ? 1 : 0;
+
+    // Crear consulta SQL para insertar los datos
+    $sql = "INSERT INTO padres_apoderados (rut, parentesco, nombre, apellido_paterno, apellido_materno, telefono_particular, telefono_trabajo, correo_electronico_particular, correo_electronico_trabajo, calle, n_calle, resto_direccion, villa_poblacion, comuna, ciudad, tutor_academico) VALUES ('$rut', '$parentesco', '$nombre', '$apellidoPaterno', '$apellidoMaterno', '$telefonoParticular', '$telefonoTrabajo', '$correoElectronicoPersonal', '$correoElectronicoTrabajo', '$calle', '$n_calle', '$restoDireccion', '$villaPoblacion', '$comuna', '$ciudad', '$tutorAcademico')";
+
+    // Ejecutar la consulta
+    if($conn->query($sql)){
+        echo "<script>alert('Datos actualizados correctamente.');</script>";
+    } else {
+        echo "<script>alert('Error al actualizar datos: " . $conn->error . "');</script>";
+    }
+}
+?>
+
+
 <div class="parents-apoderados">
     <h2>Datos padres/apoderados</h2>
     <table class="table">
@@ -18,72 +54,77 @@
     </table>
     
     <h3>Información de padres/apoderados</h3>
-    <form>
+    <form method="POST" action="padres_apoderados.php">
+
         <div class="form-group">
             <label for="rut">RUT</label>
-            <input type="text" class="form-control" id="rut">
+            <input type="text" class="form-control" name="rut">
         </div>
-        <div class="form-group">
+      <!--   <div class="form-group">
             <label for="fechaNacimiento">Fecha Nacimiento</label>
-            <input type="date" class="form-control" id="fechaNacimiento">
-        </div>
+            <input type="date" class="form-control" name="fechaNacimiento">
+        </div> -->
         <div class="form-group">
             <label for="parentesco">Parentesco</label>
-            <input type="text" class="form-control" id="parentesco">
+            <input type="text" class="form-control" name="parentesco">
         </div>
         <div class="form-group">
-            <label for="nombres">Nombres</label>
-            <input type="text" class="form-control" id="nombres">
+            <label for="nombres">Nombre</label>
+            <input type="text" class="form-control" name="nombre">
         </div>
         <div class="form-group">
             <label for="apellidoPaterno">Apellido Paterno</label>
-            <input type="text" class="form-control" id="apellidoPaterno">
+            <input type="text" class="form-control" name="apellidoPaterno">
         </div>
         <div class="form-group">
             <label for="apellidoMaterno">Apellido Materno</label>
-            <input type="text" class="form-control" id="apellidoMaterno">
+            <input type="text" class="form-control" name="apellidoMaterno">
         </div>
         <div class="form-group">
             <label for="calle">Calle</label>
-            <input type="text" class="form-control" id="calle">
+            <input type="text" class="form-control" name="calle">
         </div>
         <div class="form-group">
-            <label for="numero">N°</label>
-            <input type="text" class="form-control" id="numero">
+            <label for="n_calle">N°</label>
+            <input type="text" class="form-control" name="n_calle">
         </div>
         <div class="form-group">
             <label for="restoDireccion">Resto Dirección</label>
-            <input type="text" class="form-control" id="restoDireccion">
+            <input type="text" class="form-control" name="restoDireccion">
         </div>
         <div class="form-group">
             <label for="villaPoblacion">Villa/Población</label>
-            <input type="text" class="form-control" id="villaPoblacion">
+            <input type="text" class="form-control" name="villaPoblacion">
         </div>
         <div class="form-group">
             <label for="comuna">Comuna</label>
-            <input type="text" class="form-control" id="comuna">
+            <input type="text" class="form-control" name="comuna">
         </div>
         <div class="form-group">
             <label for="ciudad">Ciudad</label>
-            <input type="text" class="form-control" id="ciudad">
+            <input type="text" class="form-control" name="ciudad">
         </div>
         <div class="form-group">
             <label for="telefonoParticular">Teléfono Particular</label>
-            <input type="tel" class="form-control" id="telefonoParticular">
+            <input type="tel" class="form-control" name="telefonoParticular">
         </div>
         <div class="form-group">
             <label for="telefonoTrabajo">Teléfono Trabajo</label>
-            <input type="tel" class="form-control" id="telefonoTrabajo">
+            <input type="tel" class="form-control" name="telefonoTrabajo">
         </div>
         <div class="form-group">
             <label for="correoElectronicoPersonal">Correo Electrónico Personal</label>
-            <input type="email" class="form-control" id="correoElectronicoPersonal">
+            <input type="email" class="form-control" name="correoElectronicoPersonal">
         </div>
         <div class="form-group">
             <label for="correoElectronicoTrabajo">Correo Electrónico Trabajo</label>
-            <input type="email" class="form-control" id="correoElectronicoTrabajo">
+            <input type="email" class="form-control" name="correoElectronicoTrabajo">
         </div>
-        
-        <button type="submit" class="btn btn-primary btn-block custom-button">ACTUALIZAR DATOS</button>
-    </form>
+        <div class="form-group">
+            <label for="tutorAcademico">Tutor académico</label>
+            <input type="checkbox" id="tutorAcademico" name="tutorAcademico" value="1">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block custom-button" name="actualizar_datos">ACTUALIZAR DATOS</button>
+</form>
 </div>
+ 
